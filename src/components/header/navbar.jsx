9c75d49b232b624664c824988cb2203700843cf9
ChristianRiovaldo <christian.riovaldo10@gmail.com>
2024-11-05 09:52:46 +0700

    change name img file and location to assets

--- src/components/header/navbar.jsx
+++ src/components/header/navbar.jsx
@@ -3,10 +3,10 @@ import React from "react";
 // Membuat navigation bar
 const Header = () => {
     return (
-        <nav className="bg-white border-2 border-blue-950 text-blue-950 w-full h-16 fixed left-0 top-0 z-50">
-            <div className="flex flex-row justify-between items-center shadow-xl p-4">
-                <div className="ml-15">
-                    <img src="logoGamaforce.jpg" alt="Logo" className="h-10"/>
+        <nav className="bg-white border-2 border-blue-950 text-blue-950 shadow-xl flex flex-row items-center w-full h-16 fixed left-0 top-0 z-50 p-4">
+            <div className="flex flex-row justify-between items-center w-full">
+                <div className="flex justify-center items-center ml-20">
+                    <img src="./src/assets/Gamaforce.jpeg" alt="Logo Gamaforce" className="w-full h-12 p-0" />
                 </div>
                 <div className="flex flex-row justify-end items-center w-1/3 mr-20">
                     <div className="pl-2 pr-2">
